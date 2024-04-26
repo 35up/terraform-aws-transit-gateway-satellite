06e1b8c0e9994190e48564051e2eef4d4d093483
Dan Vaida <me@danvaida.com>
2024-04-26 16:44:25 +0300

    Adds support for Terraform v1.5.x

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.2"
+  required_version = "~> 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.30"
+      version = "~> 5.31"
       configuration_aliases = [
         aws.satellite,
         aws.hub,
